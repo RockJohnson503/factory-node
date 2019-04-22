44001a6cd8d54c66bb995fd8485db182be5ada67
RockJohnson503 <836867547@qq.com>
2019-04-22 14:54:16 +0800

    变更修改操作的方式

--- js/serverFunc.js
+++ js/serverFunc.js
@@ -26,14 +26,11 @@ exports.getFiler = function (request, response, fileUrl) {
     }
 
     fs.readFile(fileUrl, function (err, data) {
-        if(err){
-            response.writeHead(404, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "http://localhost:8080",
+        if(err){response.writeHead(404, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": "true"});}
+        else{
+            response.writeHead(200, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "*",
                             "Access-Control-Allow-Credentials": "true"});
-        }else{
-            response.writeHead(200, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "http://localhost:8080",
-                            "Access-Control-Allow-Credentials": "true"});
-            response.writeHead(200, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "http://localhost:8080",
-                                "Access-Control-Allow-Credentials": "true"});
 
             if(!Number(data)){
                 data = JSON.parse(data);
@@ -66,10 +63,9 @@ exports.writeFiler = function (request, response, fileUrl) {
     }
 
     fs.readFile(fileUrl, function (err, data) {
-        if(err){
-            response.writeHead(404, {'Content-Type': 'text/Json', "Access-Control-Allow-Origin": "*",
-                            "Access-Control-Allow-Credentials": "true"});
-        }else{
+        if(err){response.writeHead(404, {'Content-Type': 'text/Json', "Access-Control-Allow-Origin": "*",
+                            "Access-Control-Allow-Credentials": "true"});}
+        else{
             if(datas){
                 data = JSON.parse(data);
 
@@ -93,10 +89,10 @@ exports.writeFiler = function (request, response, fileUrl) {
 
             fs.writeFile(fileUrl, args, function (err) {
                 if(err){
-                    response.writeHead(404, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "http://localhost:8080",
+                    response.writeHead(404, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "*",
                             "Access-Control-Allow-Credentials": "true"});
                 }else{
-                    response.writeHead(200, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "http://localhost:8080",
+                    response.writeHead(200, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "*",
                             "Access-Control-Allow-Credentials": "true"});
                 }
             });
@@ -113,48 +109,102 @@ exports.changeFiler = function(request, response, fileUrl){
     var datas = JSON.parse(decodeURIComponent(args[1].split("=")[1]));
     var origin = JSON.parse(decodeURIComponent(args[0].split("=")[1]));
 
-    fs.readFile(fileUrl, function (err, data) {
-        if(err){
-            response.writeHead(404, {'Content-Type': 'text/Json', "Access-Control-Allow-Origin": "*",
-                            "Access-Control-Allow-Credentials": "true"});
-        }else{
-            data = JSON.parse(data);
+    //修改turnover
+    fs.readFile(fileUrl[0], function (err, data1) {
+        if(err){response.writeHead(404, {'Content-Type': 'text/Json', "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": "true"});}
+        else{
+            data1 = JSON.parse(data1);
 
-            for(let i in data.data){
-                if(data.data[i].factory === origin.factory && data.data[i].id === origin.id && data.data[i].name === origin.name){
-                    data.data[i].factory = datas.factory;
-                    data.data[i].id = datas.id;
-                    data.data[i].name = datas.name;
-                    data.data[i].first = datas.first;
-                    data.data[i].now = Number(datas.first) + data.data[i].in - data.data[i].out;
+            for(let i in data1.data){
+                if(data1.data[i].factory === origin.factory && data1.data[i].id === origin.id && data1.data[i].name === origin.name){
+                    for(let k in datas){
+                        if(k === "first"){
+                            datas[k] = Number(datas[k])
+                        }else{
+                            //修改detail
+                            fs.readFile(fileUrl[1], function (err, data2) {
+                                if(err){response.writeHead(404, {'Content-Type': 'text/Json', "Access-Control-Allow-Origin": "*",
+                                        "Access-Control-Allow-Credentials": "true"});}
+                                else{
+                                    data2 = JSON.parse(data2);
 
+                                    for(let q in data2.data) {
+                                        if (data2.data[q].factory === origin.factory && data2.data[q].id === origin.id && data2.data[q].name === origin.name) {
+                                            data2.data[q][k] = datas[k];
+
+                                            break;//结束循环
+                                        }
+                                    }
+
+                                    data2 = JSON.stringify(data2)
+                                    fs.writeFile(fileUrl[1], data2, function (err) {
+                                        if(err){response.writeHead(404, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "*",
+                                                    "Access-Control-Allow-Credentials": "true"});}
+                                        else{
+                                            response.writeHead(200, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "*",
+                                                    "Access-Control-Allow-Credentials": "true"});
+                                        }
+                                    });
+                                }
+                            });
+
+                            //修改keyNum
+                            fs.readFile(fileUrl[2], function (err, data3) {
+                                if(err){response.writeHead(404, {'Content-Type': 'text/Json', "Access-Control-Allow-Origin": "*",
+                                        "Access-Control-Allow-Credentials": "true"});}
+                                else{
+                                    data3 = JSON.parse(data3);
+
+                                    for(let q in data3.data) {
+                                        if (data3.data[q].factory === origin.factory && data3.data[q].id === origin.id && data3.data[q].name === origin.name) {
+                                            data3.data[q][k] = datas[k];
+
+                                            break;//结束循环
+                                        }
+                                    }
+
+                                    data3 = JSON.stringify(data3)
+                                    fs.writeFile(fileUrl[2], data3, function (err) {
+                                        if(err){response.writeHead(404, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "*",
+                                                    "Access-Control-Allow-Credentials": "true"});}
+                                        else{
+                                            response.writeHead(200, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "*",
+                                                    "Access-Control-Allow-Credentials": "true"});
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                        data1.data[i][k] = datas[k];
+                        data1.data[i].now = data1.data[i].first + data1.data[i].in - data1.data[i].out;
+                    }
                     break;//结束循环
                 }
             }
 
-            args = JSON.stringify(data);
-            fs.writeFile(fileUrl, args, function (err) {
-                if(err){
-                    response.writeHead(404, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "http://localhost:8080",
-                            "Access-Control-Allow-Credentials": "true"});
-                }else{
-                    response.writeHead(200, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "http://localhost:8080",
+            args = JSON.stringify(data1);
+            fs.writeFile(fileUrl[0], args, function (err) {
+                if(err){response.writeHead(404, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "*",
+                            "Access-Control-Allow-Credentials": "true"});}
+                else{
+                    response.writeHead(200, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "*",
                             "Access-Control-Allow-Credentials": "true"});
                 }
             });
         }
-        //  发送响应数据
-        response.end();
     });
+
+    //  发送响应数据
+    response.end();
 };
 
 //期初折账
 exports.first = function (request, response, fileUrl) {
     fs.readFile(fileUrl, function (err, data) {
-        if(err){
-            response.writeHead(404, {'Content-Type': 'text/Json', "Access-Control-Allow-Origin": "*",
-                            "Access-Control-Allow-Credentials": "true"});
-        }else{
+        if(err){response.writeHead(404, {'Content-Type': 'text/Json', "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": "true"});}
+        else{
             data = JSON.parse(data);
             data.month += 1;
             for(var i in data.data){
@@ -165,11 +215,10 @@ exports.first = function (request, response, fileUrl) {
             args = JSON.stringify(data);
 
             fs.writeFile(fileUrl, args, function (err) {
-                if(err){
-                    response.writeHead(404, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "http://localhost:8080",
-                            "Access-Control-Allow-Credentials": "true"});
-                }else{
-                    response.writeHead(200, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "http://localhost:8080",
+                if(err){response.writeHead(404, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Credentials": "true"});}
+                else{
+                    response.writeHead(200, {'Content-Type': 'text/json', "Access-Control-Allow-Origin": "*",
                             "Access-Control-Allow-Credentials": "true"});
                 }
             });
